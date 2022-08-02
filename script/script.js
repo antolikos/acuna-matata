8d567606e62ce95c2fc3effee5795d83b2dec223
Anatoly <gari8500@yandex.ru>
2022-08-02 17:06:00 +0300

    second commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//const result = confirm ("Are you here?");
+//console.log (result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+
+//const answers = [];
+//answers[0]=prompt("Как ваше имя?", " ");
+//answers[1]=prompt("Как вашa фамилия?", " ");
+//answers[2]=prompt("Как вы сюда попали?", " ");
+//console.log(typeof(answers));
+
+
+
+const user = "Ivan";
+alert(`Привет,${user}`);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
+
+    console.log(2*4==8);
